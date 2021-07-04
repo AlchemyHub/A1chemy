781fb40fa17bb5093f28c54c4d7238c183462a74
linezh <769483043@qq.com>
2021-07-04 23:53:51 +0800

    fix

--- a1chemy/data_source/csindex.py
+++ a1chemy/data_source/csindex.py
@@ -6,14 +6,30 @@ from bs4 import BeautifulSoup
 
 from a1chemy.util import write_data_to_json_file
 
+
+headers = {
+    'Connection': 'keep-alive',
+    'Cache-Control': 'max-age=0',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Language': 'en,zh-CN;q=0.9,zh;q=0.8,zh-TW;q=0.7',
+}
+
+
+def get_cookies():
+    response = requests.get('https://www.jisilu.cn/', headers=headers)
+    return response.cookies
+
 def download_csi_xls(url, xls_path):
     pattern = re.compile('.*成份列表.*')
-    html_page = requests.get(url)
+    cookies = get_cookies()
+    html_page = requests.get(url, headers=headers, cookies=cookies, verify=False)
     soup = BeautifulSoup(html_page.content, 'lxml')
     results = soup.find_all(text=pattern)
     xls_url = results[0].parent['href'] if len(results) is 1 else None
     print("xls_url=" + xls_url)
-    f = requests.get(xls_url)
+    f = requests.get(xls_url, headers=headers, cookies=cookies, verify=False)
     with open(xls_path, "wb") as target_file:
         target_file.write(f.content)
 
